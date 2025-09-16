24e4132cc18bd6ec5ff7e213ed4b6e1810efb44a
Mritunjaii <themritunjai@gmail.com>
2025-09-16 09:54:40 +0530

    Enhance Puppeteer configuration for WhatsApp client

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const { Client,LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: './sessions'   // must match Render disk mount path
-    })
+    }),
+    puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', 
+      '--disable-gpu'
+    ],
+  }
 });
 
 let latestQR = null;
